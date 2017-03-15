4e47b178b67c442efa7f56426383fcc0147054b5
Jeffrey Gilbert <visualjeff@linux.com>
2017-03-14 20:55:49 -0700

    Holy cow!  We have automated testing for plugin-testmode.js

---
+++ test/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+//
+// ScimGateway plugin startup
+// One or more plugin could be started (must be listening on unique ports)
+//
+// Could use forman module for running in separate environments
+// PM2 module for vertical clustering/loadbalancing among cpu's'
+// node-http-proxy for horizontal loadbalancing among hosts (or use nginx)
+//
+
+var testmode    = require('./lib/plugin-testmode');
+// var restful  = require('./lib/plugin-restful');
+// var forwardinc  = require('./lib/plugin-forwardinc');
+// var mssql = require('./lib/plugin-mssql');
+// var saphana   = require('./lib/plugin-saphana');
