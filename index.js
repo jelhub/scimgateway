92ec0d56526535f32e924e4ea004f4d782df9e89
Jarle Elshaug <jarle.elshaug@gmail.com>
2017-04-01 11:46:41 +0200

    loki replacing testmode

--- index.js
+++ index.js
@@ -9,8 +9,8 @@
 // node-http-proxy for horizontal loadbalancing among hosts (or use nginx)
 //
 
-var loki    = require('./lib/plugin-loki');
-// var restful  = require('./lib/plugin-restful');
-// var forwardinc  = require('./lib/plugin-forwardinc');
+var loki = require('./lib/plugin-loki');
+// var restful = require('./lib/plugin-restful');
+// var forwardinc = require('./lib/plugin-forwardinc');
 // var mssql = require('./lib/plugin-mssql');
-// var saphana   = require('./lib/plugin-saphana');
+// var saphana = require('./lib/plugin-saphana');
