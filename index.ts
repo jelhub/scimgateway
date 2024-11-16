e766365e3b05aac511a1112e1dca58da2aaf5e72
Jarle Elshaug <jarle.elshaug@gmail.com>
2024-11-16 09:07:14 +0100

    cosmetics

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
 //
-// for nodejs (version >= 22.6.0) use shebang: #!/usr/bin/env -S node --experimental-strip-types
+// for Node.js (version >= 22.6.0) use shebang: #!/usr/bin/env -S node --experimental-strip-types
 //
 // SCIM Gateway plugin startup
 // One or more plugin can be started having unique port listener (configuration scimgateway.port)
@@ -9,10 +9,10 @@
 // example starting all default plugins:
 // const plugins = ['loki', 'scim', 'entra-id', 'ldap', 'mssql', 'api', 'mongodb', 'saphana', 'soap']
 // 
-// some plugins may require modules to be installed e.g.,:
-// soap    - bun/npm install soap
-// mssql   - bun/npm install tedious
-// saphana - bun/npm install hdb
+// some plugin requires module to be installed e.g.,:
+// plugin-soap: bun install soap
+// plugin-mssql: bun install tedious
+// plugin-saphana - bun install hdb
 //
 
 const plugins = ['loki']
