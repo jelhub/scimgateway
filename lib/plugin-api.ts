b5cc0836b8761bd4cf2d1aba8db7c8986d311b4b
Jarle Elshaug <jarle.elshaug@gmail.com>
2025-09-22 10:01:32 +0200

    new method publicApi and respons body format changes

--- lib/plugin-api.ts
+++ lib/plugin-api.ts
@@ -1,19 +1,13 @@
 // =================================================================================
-// File:    plugin-api.js
+// File:    plugin-api.ts
 //
 // Author:  Jarle Elshaug
 //
 // Purpose: Demonstrate scimgateway api functionality by using a REST based plugin
-//          Using /api ScimGateway transfer "as is" to plugin and returns plugin result by adding
-//          {"meta": {"result": "success"}}
-//          or
-//          {"meta": {"result": "error"}}
-//
+//          Using /api scimgateway transfer "as is" to plugin and returns plugin result
 //          This plugin becomes what you it to be
 //
-// Test prereq: Internet connection towards baseUrl defined for testing purpose (http://fakerestapi.azurewebsites.net)
-//
-// Supported by scimgateway:
+// Supported methods:
 //  GET /api
 //  GET /api?queries
 //  GET /api/{id}
@@ -21,6 +15,7 @@
 //  PUT /api/{id} + body
 //  PATCH /api/{id} + body
 //  DELETE /api/{id}
+//  <method> /pub/api - '/pub/api' is the publicApi path having no authentication - all methods supported - using scimgateway.publicApi()
 //
 // =================================================================================
 
@@ -58,23 +53,21 @@ const helper = new HelperRest(scimgateway)
 // post http://localhost:8890/api
 // body = {"title":"BMW X5","price":58}
 //
-scimgateway.postApi = async (baseEntity, apiObj, ctx) => {
+scimgateway.postApi = async (baseEntity, body, ctx) => {
   const action = 'postApi'
-  scimgateway.logDebug(baseEntity, `handling ${action} apiObj=${JSON.stringify(apiObj)}`)
+  scimgateway.logDebug(baseEntity, `handling ${action} body=${JSON.stringify(body)}`)
 
-  if ((typeof (apiObj) !== 'object') || (Object.keys(apiObj).length === 0)) {
+  if ((typeof (body) !== 'object') || (Object.keys(body).length === 0)) {
     throw new Error('unsupported POST syntax')
   }
 
   const method = 'POST'
   const path = '/products/add'
-  const body = apiObj
   try {
     const response = await helper.doRequest(baseEntity, method, path, body, ctx)
     return response.body
   } catch (err) {
-    const newErr = err
-    throw newErr
+    throw err
   }
 }
 
@@ -86,17 +79,16 @@ scimgateway.postApi = async (baseEntity, apiObj, ctx) => {
 // put http://localhost:8890/api/100
 // body = {"title":"BMW X1","price":21}
 //
-scimgateway.putApi = async (baseEntity, id, apiObj, ctx) => {
+scimgateway.putApi = async (baseEntity, id, body, ctx) => {
   const action = 'putApi'
-  scimgateway.logDebug(baseEntity, `handling ${action} id=${id} apiObj=${JSON.stringify(apiObj)}`)
+  scimgateway.logDebug(baseEntity, `handling ${action} id=${id} body=${JSON.stringify(body)}`)
 
-  if ((typeof (apiObj) !== 'object') || (Object.keys(apiObj).length === 0)) {
+  if ((typeof (body) !== 'object') || (Object.keys(body).length === 0)) {
     throw new Error('unsupported PUT syntax')
   }
 
   const method = 'PUT'
   const path = `/products/${id}`
-  const body = apiObj
   try {
     const response = await helper.doRequest(baseEntity, method, path, body, ctx)
     return response.body
@@ -114,24 +106,22 @@ scimgateway.putApi = async (baseEntity, id, apiObj, ctx) => {
 // patch http://localhost:8890/api/100
 // body = {"title":"BMW X3"}
 //
-scimgateway.patchApi = async (baseEntity, id, apiObj, ctx) => {
+scimgateway.patchApi = async (baseEntity, id, body, ctx) => {
   const action = 'patchApi'
-  scimgateway.logDebug(baseEntity, `handling ${action} id=${id} apiObj=${JSON.stringify(apiObj)}`)
+  scimgateway.logDebug(baseEntity, `handling ${action} id=${id} body=${JSON.stringify(body)}`)
 
-  if ((typeof (apiObj) !== 'object') || (Object.keys(apiObj).length === 0)) {
+  if ((typeof (body) !== 'object') || (Object.keys(body).length === 0)) {
     throw new Error('unsupported PATCH syntax')
   }
 
   const method = 'PATCH'
   const path = `/products/${id}`
-  const body = apiObj
 
   try { // note, Books example do not support patch
     const response = await helper.doRequest(baseEntity, method, path, body, ctx)
     return response.body
   } catch (err) {
-    const newErr = err
-    throw newErr
+    throw err
   }
 }
 
@@ -143,9 +133,9 @@ scimgateway.patchApi = async (baseEntity, id, apiObj, ctx) => {
 // get http://localhost:8890/api
 // get http://localhost:8890/api/100
 //
-scimgateway.getApi = async (baseEntity, id, apiQuery, apiObj, ctx) => {
+scimgateway.getApi = async (baseEntity, id, query, ctx) => {
   const action = 'getApi'
-  scimgateway.logDebug(baseEntity, `handling ${action} id=${id} apiQuery=${JSON.stringify(apiQuery)} apiObj=${JSON.stringify(apiObj)}`)
+  scimgateway.logDebug(baseEntity, `handling ${action} id=${id} apiQuery=${JSON.stringify(query)}}`)
 
   const method = 'GET'
   let path = '/products'
@@ -159,8 +149,7 @@ scimgateway.getApi = async (baseEntity, id, apiQuery, apiObj, ctx) => {
     const response = await helper.doRequest(baseEntity, method, path, body, ctx)
     return response.body
   } catch (err) {
-    const newErr = err
-    throw newErr
+    throw err
   }
 }
 
@@ -183,8 +172,63 @@ scimgateway.deleteApi = async (baseEntity, id, ctx) => {
     const response = await helper.doRequest(baseEntity, method, path, body, ctx)
     return response.body
   } catch (err) {
-    const newErr = err
-    throw newErr
+    throw err
+  }
+}
+
+// =================================================
+// publicApi
+// public available - no authentication
+// =================================================
+//
+// example:
+// GET http://localhost:8890/pub/api/bmw?model=BMW X3
+// POST http://localhost:8890/pub/api - body={"model":"BMW X3"}
+// PATCH http://localhost:8890/pub/api/bmw - body={"model":"BMW X5"}
+// PUT http://localhost:8890/pub/api/bmw - body={"model":"BMW X3"}
+// DELETE http://localhost:8890/pub/api/bmw
+//
+const getPublicApi = async (baseEntity: string, id: string | undefined, query: Record<string, any> | undefined) => {
+  return { get: { baseEntity, id, query } }
+}
+
+const postPublicApi = async (baseEntity: string, body: any) => {
+  if (!body) throw new Error('POST is missing mandatory body')
+  return { post: { body } }
+}
+
+const patchPublicApi = async (baseEntity: string, id: string | undefined, body: any) => {
+  if (!id || !body) throw new Error('PATCH is missing mandatory id/body')
+  return { patch: { id, body } }
+}
+
+const putPublicApi = async (baseEntity: string, id: string | undefined, body: any) => {
+  if (!id || !body) throw new Error('PUT is missing mandatory id/body')
+  return { put: { id, body } }
+}
+
+const deletePublicApi = async (baseEntity: string, id: string | undefined, body: any) => {
+  if (!id) throw new Error('DELETE is missing mandatory id')
+  return { delete: { id, body } }
+}
+
+scimgateway.publicApi = async (baseEntity, method, id, query, body) => {
+  const action = 'publicApi'
+  scimgateway.logDebug(baseEntity, `handling ${action} method=${method} id=${id} query=${query ? JSON.stringify(query) : query} body=${JSON.stringify(body)}`)
+
+  switch (method) {
+    case 'GET':
+      return await getPublicApi(baseEntity, id, query)
+    case 'POST':
+      return await postPublicApi(baseEntity, body)
+    case 'PATCH':
+      return await patchPublicApi(baseEntity, id, body)
+    case 'PUT':
+      return await putPublicApi(baseEntity, id, body)
+    case 'DELETE':
+      return await deletePublicApi(baseEntity, id, body)
+    default:
+      throw new Error(`${action} method ${method} is not supported`)
   }
 }
 
